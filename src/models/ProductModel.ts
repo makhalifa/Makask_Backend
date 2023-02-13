722147909d827e542d3f6f467cdf9ae713c308a4
Mohamed <7mdiano@gmail.com>
2023-02-13 21:22:49 +0200

    edit pricing

--- src/models/ProductModel.ts
+++ src/models/ProductModel.ts
@@ -3,10 +3,21 @@ import { Schema, model, Types } from 'mongoose'
 const productSchema = new Schema(
     {
         // _id: { type: String, required: true, unique: true },
-        // sellerid: { type: Types.ObjectId, required: true, ref: 'sellers' },
+        sellerid: { type: Types.ObjectId, required: true, ref: 'sellers' },
 
         title: { type: String, required: true },
-        price: { type: Number, required: true },
+        pricing: {
+            type: {
+                price: { type: Number, required: true },
+                discount: { type: Number, required: true },
+                // discount_type: { type: String, required: true },
+                // discount_value: { type: Number, required: true },
+                // discount_start_date: { type: Date, required: true },
+                // discount_end_date: { type: Date, required: true },
+                dicountedPrice: { type: Number, required: true },
+            },
+            required: true,
+        },
         category: { type: String, required: true },
         sub_category: { type: String, required: true },
         // description: { type: String },
