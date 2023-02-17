2eaf7676d31a3f01d15d3411a1f63b8360a224f1
Mohamed <7mdiano@gmail.com>
2023-02-17 17:00:41 +0200

    edit getProducts & Filter

--- src/controller/ProductController.ts
+++ src/controller/ProductController.ts
@@ -3,8 +3,16 @@ import { faker } from '@faker-js/faker'
 import Product from '../models/ProductModel'
 
 const getProducts = async (req: Request, res: Response) => {
-    const products = await Product.find()
-    res.json(products)
+    try {
+        const limit = req.query.limit ? parseInt(req.query.limit as string) : 10
+        const page = req.query.page ? parseInt(req.query.page as string) : 1
+        const products = await Product.find()
+            .limit(limit)
+            .skip(limit * (page - 1))
+        res.json(products)
+    } catch (error) {
+        res.json(error).status(500)
+    }
 }
 
 const getProductsById = async (req: Request, res: Response) => {
@@ -41,57 +49,122 @@ const getProductsByCategoryAndSubCategory = async (req: Request, res: Response) 
     res.json(products)
 }
 
-const getProductsBySearch = async (req: Request, res: Response) => {
+const filterProducts = async (req: Request, res: Response) => {
+
+    /**
+     * Filter
+     * title: string
+     * category: string | string[]
+     * subcategory: string | string[]
+     * color: string | string[]
+     * brand: string | string[]
+     * size: string | string[]
+     * min_price: number
+     * max_price: number
+     * 
+     * Pagination
+     * limit: number
+     * page: number
+     * 
+     * Sort
+     * sort: string
+     */
+
+    // Filter
+    const { title, category, subcategory, color, brand, sort, size } = req.query
+    const min_price = parseFloat(req.query.min_price as string)
+    const max_price = parseFloat(req.query.max_price as string)
+
+    // Pagination
+    const limit = req.query.limit ? parseInt(req.query.limit as string) : 10
+    const page = req.query.page ? parseInt(req.query.page as string) : 1
+    const skip = limit * (page - 1)
+
+    // Sort
+    const sortQuery = {} as any
+    if (sort) {
+        if (sort === 'price_asc') {
+            sortQuery['pricing.price'] = 1
+        }
+        if (sort === 'price_desc') {
+            sortQuery['pricing.price'] = -1
+        }
+    }
+
+    const options = { skip, limit, sort: sortQuery }
+    
+    const query = {} as any
+    if (Object.keys(req.query).length == 0) {
+        return res.status(400).json({ message: 'No query' })
+    }
+    if (title) {
+        query.title = { $regex: title, $options: 'i' }
+    }
+    if (category) {
+        if (Array.isArray(category)) {
+            const categoryRegex = category.map(cat => new RegExp(cat as any, 'i'))
+            query.category = { $in: categoryRegex}
+        } else {
+            query.category = {$regex: category, $options: 'i'}
+        }
+    } 
+    if (subcategory) {
+        if (Array.isArray(subcategory)) {
+            const subcategoryRegex = subcategory.map(subcat => new RegExp(subcat as any, 'i'))
+            query.subcategory = { $in: subcategoryRegex}
+        } else {
+            query.subcategory = { $regex: subcategory, $options: 'i'}
+        }
+    }
+    if (!isNaN(min_price) && !isNaN(max_price)) {
+        query['pricing.price'] = { $gte: min_price, $lte: max_price }
+    } else {
+        if (!isNaN(min_price)) {
+            query['pricing.price'] = { $gte: min_price }
+        }
+        if (!isNaN(max_price)) {
+            query['pricing.price'] = { $lte: max_price }
+        }
+    }
+    if (color) {
+        if (Array.isArray(color)) {
+            query['color.color'] = { $in: color }
+        } else {
+            query['color.color'] = color
+        }
+    }
+    if (brand) {
+        if (Array.isArray(brand)) {
+            query['brand.brand'] = { $in: brand }
+        } else {
+            query['brand.brand'] = brand
+        }
+    }
+    if (size) {
+        if (Array.isArray(size)) {
+            query['size.size'] = { $in: size }
+        } else {
+            query['size.size'] = size
+        }
+    }
+
     try {
-        const products = await Product.find({
-            title: { $regex: req.query.title, $options: 'i' },
-        })
+        const products = await Product.find(query, {}, options)
         res.json(products)
     } catch (error) {
         res.json(error).status(500)
     }
 }
 
-const filterProducts = async (req: Request, res: Response) => {
-    // const { title, category, sub_category, min_price, max_price } = req.query
-    // check if the query is empty
-    if (Object.keys(req.query).length === 0) {
-        res.status(400).json({ message: 'No query' })
-    } else {
-        let products
-        // check if the query has the
-        if (req.query.title) {
-            // check if the query.title is a substring of the product.title
-            products = await Product.find({
-                title: { $regex: req.query.title, $options: 'i' },
-            })
-        } else {
-            products = await Product.find()
-        }
-        if (req.query.category) {
-            // from the products, filter by category and ignore case
-            products = products.filter((product) =>
-                product.category.toLowerCase().includes((req.query.category as string).toLowerCase())
-            )
-        }
-        if (req.query.subcategory) {
-            // from the products, filter by sub_category
-            products = products.filter((product) => 
-                product.subcategory==req.query.subcategory
-            )
-        }
-        if (req.query.min_price) {
-            // from the products, filter by price
-            products = products.filter(
-                (product) => product.pricing.price >= parseInt(req.query.min_price as string)
-            )
-        }
-        if (req.query.max_price) {
-            products = products.filter(
-                (product) => product.pricing.price <= parseInt(req.query.max_price as string)
-            )
-        }
+const getBestSeller = async (req: Request, res: Response) => {
+    try {
+        const limit = req.query.limit ? parseInt(req.query.limit as string) : 10
+        const page = req.query.page ? parseInt(req.query.page as string) : 1
+        const skip = limit * (page - 1)
+        const products = await Product.find().sort({ sold: -1 , rating:-1}).limit(limit).skip(skip)
         res.json(products)
+    } catch (error) {
+        res.json(error).status(500)
     }
 }
 
@@ -111,16 +184,15 @@ const deleteProduct = async (req: Request, res: Response) => {
     })
 }
 
-
 export default {
     getProducts,
     createProduct,
     getProductsByCategory,
     getProductsBySubCategory,
     getProductsByCategoryAndSubCategory,
-    getProductsBySearch,
     filterProducts,
     getProductsById,
     updateProduct,
     deleteProduct,
+    getBestSeller
 }
