9a165310f54de47e20367fb151e7ad9f6f27b747
Mohamed <7mdiano@gmail.com>
2023-02-17 17:01:30 +0200

    add best_seller

--- src/routes/ProductRouter.ts
+++ src/routes/ProductRouter.ts
@@ -5,14 +5,16 @@ const router = express.Router()
 
 router.get('/', ProductController.getProducts)
 router.post('/', ProductController.createProduct)
-// router.get('/random', ProductController.randomProduct)
-router.get('/search', ProductController.getProductsBySearch)
 router.get('/category/:category', ProductController.getProductsByCategory)
 router.get('/sub_category/:sub_category', ProductController.getProductsBySubCategory)
 router.get('/category/:category/sub_category/:sub_category', ProductController.getProductsByCategoryAndSubCategory)
 router.get('/filter', ProductController.filterProducts)
+router.get('/best_seller', ProductController.getBestSeller)
+// router.get('/best_deal', ProductController.getBestDeal)
+// router.get('/new_arrival', ProductController.getNewArrival)
 router.put('/:id', ProductController.updateProduct)
 router.get('/:id', ProductController.getProductsById)
+// router.get('/:id/reviews', ProductController.getReviews)
 router.delete('/:id', ProductController.deleteProduct)
 
 export default router
