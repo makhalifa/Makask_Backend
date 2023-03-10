e2f7941487a471897346faeaa41fcaa7f9b3fd48
Mohamed <7mdiano@gmail.com>
2023-03-10 19:59:59 +0200

    add return Schema

---
+++ src/models/Order/ReturnModel.ts
@@ -0,0 +1,15 @@
+import { Schema, Types, model, Model } from 'mongoose'
+
+const ReturnSchema = new Schema({
+    order: { type: Types.ObjectId, ref: 'orders' },
+    customer: {type: Types.ObjectId,ref:'customers'},
+    order_item: { type: Types.ObjectId, ref: 'order_items' },
+    request_status: { type: String, require: true, enum: ['Pending', 'Accepted', 'Refused'] },
+    res_date: { type: Date },
+    created_at: { type: Date, default: Date.now()},
+})
+
+
+const ReturnModel = new Model('returns', ReturnSchema)
+
+export default ReturnModel
